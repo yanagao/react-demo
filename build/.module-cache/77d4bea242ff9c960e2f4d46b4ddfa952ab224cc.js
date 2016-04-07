ea4bd5ca3063bee16f971616467c62c73062f9cc
yana <yanagaoy@gmail.com>
2016-04-07 09:56:54 +0800

    动画

--- build/.module-cache/77d4bea242ff9c960e2f4d46b4ddfa952ab224cc.js
+++
@@ -1,9 +0,0 @@
-var HelloMessage = React.createClass({displayName: "HelloMessage",
-    render: function() {
-        return React.createElement("h1", null, "Hello ", this.props.name);
-    }
-});
-ReactDOM.render(
-    React.createElement(HelloMessage, {name: "yana"}),
-    document.getElementById('example')
-);
